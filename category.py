4f6de7a9f03d2d471e7170c610ea86527c240555
luciekle <lucietrashbin@gmail.com>
2023-01-25 11:15:38 +0100

    es workt nicht :(

--- category.py
+++ category.py
@@ -1,34 +1,44 @@
-import tracker, json
+import tracker, csv
 from datetime import datetime
 
 class category:
     """Methods for handeling everything related to tracking a single category"""
 
     def __init__(self, name):
-        #TODO: make sure name is a string
+        #TODO: sicherstellen, dass name ein String ist
         self.name = name
         self.t = tracker()
         
-    
+        
     def start(self):
-        print(f"Hallo!\n")
+        with open("categories.csv", "a+") as cat_list:
+            previous_cats = next(csv.reader(cat_list))
+            print(f"Hallo!\n")
+        
         go = input("Möchtest du eine neue Kategorie tracken? [y/n]")
         if (go == "y"):
-            #TODO: Prüfen, ob Kategorie schon existiert
             cat_name = input("Was möchtest du tracken?")
-            cat = category(cat_name)
+            # Prüfe, ob eine Kategorie schon existiert
+            while (cat_name in cat_list):
+                print("Sorry, diese Kategorie gibt es schon!")
+                cat_name = input("Was möchtest du tracken?")  
+            
+            cat = category(cat_name) 
+            cat_list.writerow(previous_cats, cat_name)
+            print(f"Alles klar! Du kannst jetzt {cat_name} tracken")
     
         go = input("Möchtest du für heute etwas tracken? [y/n]")
         if (go == "y"):
             #TODO: cat_list printen mit Zahlen
-            #for (i in cat_list) print(f"[\i] \cat_list[i]")
+            for i in cat_list: print(f"{i} - {cat_list[i]}")
+            #TODO: nur Zahlen annehmen 
             cat_number = input("Bitte wähle deine Kategorie:")
-            #cat = cat_list[cat_number]
+            cat = cat_list[cat_number]
             cat.add_today()
 
         go = input("Möchtest du deine Gewohnheiten analysieren? [y/n]")
         if (go == "y"):
-            print("[1] Graph")
+            print(f"[1] Graph \n[2] anderes cooles Diagramm") # Die Darstellungsformen kann man ja einfach hardcoden
             vis = input("Wie möchtest du deine Daten darstellen?")
             # TODO: Go Nicole!
         
